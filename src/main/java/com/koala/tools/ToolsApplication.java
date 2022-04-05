1ac32922c668d940a450c30e3b09008d9add6a36
KoalaBear <owen000814@outlook.com>
2022-04-05 11:24:15 +0800

    feat: update

--- src/main/java/com/koala/tools/ToolsApplication.java
+++ src/main/java/com/koala/tools/ToolsApplication.java
@@ -1,16 +1,36 @@
 package com.koala.tools;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
  * @author koala
  */
+@Slf4j
 @SpringBootApplication
 public class ToolsApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ToolsApplication.class, args);
+    public static void main(String[] args) throws UnknownHostException {
+        ConfigurableApplicationContext application = SpringApplication.run(ToolsApplication.class, args);
+        Environment env = application.getEnvironment();
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        String port = env.getProperty("server.port");
+        String property = env.getProperty("server.servlet.context-path");
+        String path = property == null ? "" : property;
+        log.info(
+                "\n\t" +
+                        "----------------------------------------------------------\n\t" +
+                        "Application Tools is running! Access URLs:\n\t" +
+                        "Local: \t\thttp://localhost:" + port + path + "/\n\t" +
+                        "External: \thttp://" + ip + ":" + port + path + "/\n\t" +
+                        "------------------------------------------------------------");
+
     }
 
 }
