aae9183ddab266ad6a93e1a171a4fd8b34e43e2e
KoalaBear <owen000814@outlook.com>
2022-04-05 19:37:13 +0800

    feat: update

--- src/main/java/com/koala/tools/config/CoreWebConfig.java
+++ src/main/java/com/koala/tools/config/CoreWebConfig.java
@@ -10,6 +10,7 @@ import org.springframework.http.converter.json.MappingJackson2HttpMessageConvert
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 
@@ -22,6 +23,7 @@ import java.util.List;
  * @description
  */
 @Configuration
+@EnableWebMvc
 @DependsOn({"beanContext"})
 public class CoreWebConfig implements WebMvcConfigurer {
 
