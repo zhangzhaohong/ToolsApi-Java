ad9847f44c0c64fc43be00047391119ac0d227ce
KoalaBear <owen000814@outlook.com>
2022-06-22 17:20:35 +0800

    feat: update

--- src/main/java/com/koala/tools/controller/PixieeController.java
+++ src/main/java/com/koala/tools/controller/PixieeController.java
@@ -1,24 +1,31 @@
 package com.koala.tools.controller;
 
-import com.google.gson.Gson;
+import com.alibaba.excel.EasyExcelFactory;
+import com.alibaba.excel.read.listener.PageReadListener;
 import com.koala.tools.http.annotation.MixedHttpRequest;
-import com.koala.tools.mail.EmailSenderService;
-import com.koala.tools.mail.EmailService;
+import com.koala.tools.mail.EmailExecutorService;
 import com.koala.tools.mail.MailDataContext;
 import com.koala.tools.models.RespModel;
+import com.koala.tools.models.mail.UserListModel;
 import com.koala.tools.models.pixiee.ProductInfoModel;
 import com.koala.tools.utils.GsonUtil;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.util.FileSystemUtils;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
-import javax.mail.MessagingException;
-import java.io.UnsupportedEncodingException;
-import java.util.HashMap;
-import java.util.UUID;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * @author koala
@@ -26,12 +33,19 @@ import java.util.UUID;
  * @date 2022/6/20 11:07
  * @description
  */
+@Slf4j
 @RestController
 @RequestMapping("tools/pixiee")
 public class PixieeController {
 
+    @Value("${spring.mail.properties.tmpPath}")
+    private String tmpPath;
+
     @Resource
-    private EmailSenderService emailSenderService;
+    private EmailExecutorService emailExecutorService;
+
+    @Resource
+    private RedisTemplate<String, Object> redisTemplate;
 
     @PostMapping(value = "getInfo", produces = {"application/json;charset=utf-8"})
     public String getInfo(@MixedHttpRequest String description, @MixedHttpRequest String material, @MixedHttpRequest String packageInfo, @MixedHttpRequest String pockets, @MixedHttpRequest String type, @MixedHttpRequest String caring) {
@@ -41,16 +55,96 @@ public class PixieeController {
     @PostMapping(value = "sendMail", produces = {"application/json;charset=utf-8"})
     public String sendMail(
             @MixedHttpRequest String to,
+            @MixedHttpRequest(required = false) String replyTo,
             @MixedHttpRequest String subject,
             @MixedHttpRequest String text,
-            @MixedHttpRequest(required = false) MultipartFile file
+            @MixedHttpRequest Integer type,
+            @MixedHttpRequest(required = false) MultipartFile file1,
+            @MixedHttpRequest(required = false) MultipartFile file2,
+            @MixedHttpRequest(required = false) MultipartFile file3,
+            @MixedHttpRequest(required = false) MultipartFile file4,
+            @MixedHttpRequest(required = false) MultipartFile file5
     ) {
         String uuid = UUID.randomUUID().toString();
-        emailSenderService.addTask(new MailDataContext(uuid, 0L, to, subject, text, null));
+        ArrayList<String> fileList = new ArrayList<>(0);
+        for (MultipartFile multipartFile : Arrays.asList(file1, file2, file3, file4, file5)) {
+            fileList = save2TmpFile(uuid, multipartFile, fileList);
+        }
+        emailExecutorService.addTask(new MailDataContext(tmpPath, uuid, 0, type, to, replyTo, subject, text, fileList));
         HashMap<String, Object> result = new HashMap<>(0);
         result.put("taskId", uuid);
-        result.put("taskLength", 0);
+        result.put("taskLength", 1);
         return GsonUtil.toString(new RespModel(200, "add task success", result));
+    }
+
+    @PostMapping(value = "sendMailWithXlsx", produces = {"application/json;charset=utf-8"})
+    public String sendMailWithXlsx(
+            @MixedHttpRequest MultipartFile userDataFile,
+            @MixedHttpRequest(required = false) String replyTo,
+            @MixedHttpRequest String subject,
+            @MixedHttpRequest String text,
+            @MixedHttpRequest Integer type,
+            @MixedHttpRequest(required = false) MultipartFile file1,
+            @MixedHttpRequest(required = false) MultipartFile file2,
+            @MixedHttpRequest(required = false) MultipartFile file3,
+            @MixedHttpRequest(required = false) MultipartFile file4,
+            @MixedHttpRequest(required = false) MultipartFile file5
+    ) {
+        AtomicReference<Integer> taskLength = new AtomicReference<>(0);
+        String uuid = UUID.randomUUID().toString();
+        ArrayList<String> userDataList = new ArrayList<>(1);
+        userDataList = save2TmpFile(uuid, userDataFile, userDataList);
+        ArrayList<String> fileList = new ArrayList<>(0);
+        for (MultipartFile multipartFile : Arrays.asList(file1, file2, file3, file4, file5)) {
+            fileList = save2TmpFile(uuid, multipartFile, fileList);
+        }
+        if (!userDataList.isEmpty()) {
+            File userDataOriginFile = new File(userDataList.get(0));
+            ArrayList<String> finalFileList = fileList;
+            EasyExcelFactory.read(userDataOriginFile, UserListModel.class, new PageReadListener<UserListModel>(dataList -> {
+                for (UserListModel userdata : dataList) {
+                    Integer tmp = taskLength.updateAndGet(v -> v + 1);
+                    emailExecutorService.addTask(new MailDataContext(tmpPath, uuid, tmp, type, userdata.getEmailAddress(), Objects.equals(replyTo, "") ? null : replyTo, subject, text, finalFileList));
+                }
+            })).sheet("Sheet1").doRead();
+        }
+        redisTemplate.opsForValue().set(String.format("task:length:%s", uuid), taskLength.get(), 12L, TimeUnit.HOURS);
+        HashMap<String, Object> result = new HashMap<>(0);
+        result.put("taskId", uuid);
+        result.put("taskLength", taskLength.get());
+        return GsonUtil.toString(new RespModel(200, "add task success", result));
+    }
+
+    @GetMapping(value = "getStatus", produces = {"application/json;charset=utf-8"})
+    public String getStatus(
+            @MixedHttpRequest(required = true) String taskId
+    ) {
+        HashMap<String, Object> result = new HashMap<>(0);
+        result.put("taskId", taskId);
+        result.put("finished", redisTemplate.opsForValue().get(String.format("task:%s:finished", taskId)));
+        result.put("taskLength", redisTemplate.opsForValue().get(String.format("task:length:%s", taskId)));
+        return GsonUtil.toString(new RespModel(200, "add task success", result));
+    }
+
+    private ArrayList<String> save2TmpFile(String taskId, MultipartFile file, ArrayList<String> fileList) {
+        if (Objects.isNull(file) || file.isEmpty()) {
+            return fileList;
+        }
+        File folder = new File(String.format("%s/%s", tmpPath, taskId));
+        if  (!folder.exists()  && !folder.isDirectory()){
+            folder.mkdirs();
+        }
+        String filePath = String.format("%s/%s/%s", tmpPath, taskId, file.getOriginalFilename());
+        File dest = new File(filePath);
+        try {
+            file.transferTo(dest);
+        } catch (IOException e) {
+            log.error(e.toString(), e);
+        }
+        if (!StringUtils.isEmpty(filePath)) {
+            fileList.add(filePath);
+        }
+        return fileList;
     }
 
 }
--- src/main/java/com/koala/tools/mail/EmailSenderService.java
+++ src/main/java/com/koala/tools/mail/EmailSenderService.java
@@ -1,100 +1,100 @@
 package com.koala.tools.mail;
 
-import com.google.common.util.concurrent.ListeningExecutorService;
-import com.google.common.util.concurrent.MoreExecutors;
-import com.koala.tools.utils.GsonUtil;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.RandomUtils;
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
-import java.util.Objects;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+import java.util.Optional;
 
 /**
  * @author koala
  * @version 1.0
- * @date 2022/6/22 13:55
+ * @date 2022/6/22 13:19
  * @description
  */
-@Slf4j
-@Service
-public class EmailSenderService implements InitializingBean {
-
-    private final String mailSenderRedisKey = "mail:sender:exec:list";
-
+@Component
+public class EmailSenderService {
     @Resource
-    private RedisTemplate<String, Object> redisTemplate;
+    private JavaMailSender javaMailSender;
 
-    @Resource
-    private EmailService emailService;
+    @Value("${spring.mail.username}")
+    private String from;
 
-    private volatile Boolean runLoopFlag = true;
+    @Value("${spring.mail.properties.nickName}")
+    private String fromName;
 
-    private final ListeningExecutorService executorService =
-            executorService("mail-sender-service", 5, 10);
-
-    public static ListeningExecutorService executorService(String namePrefix, Integer corePoolSize, Integer maxPoolSize) {
-        log.info("perf exec executor info = corePoolSize: {}, maxPoolSize: {}", corePoolSize, maxPoolSize);
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.initialize();
-        //核心线程池大小
-        executor.setCorePoolSize(corePoolSize);
-        //缓存区大小
-        executor.setQueueCapacity(0);
-        //线程池不要太大，防止影响主业务
-        executor.setMaxPoolSize(maxPoolSize);
-        //存活时间
-        executor.setKeepAliveSeconds(30);
-        //线程前缀
-        executor.setThreadNamePrefix(namePrefix);
-        //拒绝策略
-        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
-        // 创建线程池
-        return MoreExecutors.listeningDecorator(executor.getThreadPoolExecutor());
+    /**
+     * 发送纯文本邮件.
+     *
+     * @param to      目标email 地址
+     * @param subject 邮件主题
+     * @param text    纯文本内容
+     */
+    public void sendMail(String to, String replyTo, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(from);
+        message.setTo(to);
+        message.setReplyTo(Optional.ofNullable(replyTo).orElse(from));
+        message.setSubject(subject);
+        message.setText(text);
+        javaMailSender.send(message);
     }
 
-    public void addTask(MailDataContext mailDataContext) {
-        redisTemplate.opsForList().leftPush(mailSenderRedisKey, GsonUtil.toString(mailDataContext));
-    }
-
-    public void scanRedis() {
-        while (Boolean.TRUE.equals(runLoopFlag) || executorService.isTerminated()) {
-            try {
-                String result = String.valueOf(redisTemplate.opsForList().rightPop(mailSenderRedisKey));
-                if (Objects.isNull(result) || StringUtils.isEmpty(result) || result.equals("null")) {
-                    TimeUnit.SECONDS.sleep(RandomUtils.nextInt(1, 3));
-                    continue;
-                }
-                MailDataContext mailDataContext = GsonUtil.toBean(result, MailDataContext.class);
-                executorService.execute(() -> sendMail(mailDataContext));
-            } catch (Exception e) {
-                log.error("服务异常", e);
-            }
+    /**
+     * 发送邮件并携带附件.
+     * 请注意 from 、 to 邮件服务器是否限制邮件大小
+     *
+     * @param to      目标email 地址
+     * @param subject 邮件主题
+     * @param text    纯文本内容
+     * @param fileList    附件的路径 当然你可以改写传入文件
+     */
+    public void sendMailWithAttachment(String to, String replyTo, String subject, String text, List<String> fileList) throws MessagingException, UnsupportedEncodingException {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+        helper.setFrom(from, fromName);
+        helper.setTo(to);
+        helper.setReplyTo(Optional.ofNullable(replyTo).orElse(from));
+        helper.setSubject(subject);
+        helper.setText(text);
+        for (String filePath: fileList) {
+            File attachment = new File(filePath);
+            helper.addAttachment(attachment.getName(), attachment);
         }
+        javaMailSender.send(mimeMessage);
     }
 
-    private void sendMail(MailDataContext mailDataContext) {
-        mailDataContext.startExec(emailService);
+    /**
+     * 发送富文本邮件.
+     *
+     * @param to       目标email 地址
+     * @param subject  邮件主题
+     * @param text     纯文本内容
+     * @param fileList 附件的路径 当然你可以改写传入文件
+     */
+    public void sendRichMail(String to, String replyTo, String subject, String text, List<String> fileList) throws MessagingException {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+        helper.setFrom(from);
+        helper.setTo(to);
+        helper.setReplyTo(Optional.ofNullable(replyTo).orElse(from));
+        helper.setSubject(subject);
+        helper.setText(text, true);
+        // 图片占位写法  如果图片链接写入模板 注释下面这一行
+        // helper.addInline("qr", new FileSystemResource(filePath));
+        for (String filePath: fileList) {
+            File attachment = new File(filePath);
+            helper.addAttachment(attachment.getName(), attachment);
+        }
+        javaMailSender.send(mimeMessage);
     }
 
-    public void stopLoop() {
-        this.runLoopFlag = false;
-        log.warn("～～～服务停止中～～～");
-    }
-
-
-    @Override
-    public void afterPropertiesSet() {
-        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(1, 1, 5000, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<>(5), new ThreadPoolExecutor.AbortPolicy());
-        threadPoolExecutor.submit(this::scanRedis, "EmailSenderService");
-        threadPoolExecutor.shutdown();
-        Runtime.getRuntime().addShutdownHook(new Thread(this::stopLoop));
-    }
 }
