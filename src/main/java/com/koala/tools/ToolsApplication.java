f2657629a4dd48596dd2fa3d5f45ce58ed453c9c
KoalaBear <owen000814@outlook.com>
2022-06-27 17:00:43 +0800

    feat: update

--- src/main/java/com/koala/tools/ToolsApplication.java
+++ src/main/java/com/koala/tools/ToolsApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -31,6 +33,14 @@ public class ToolsApplication {
                         "External: \thttp://" + ip + ":" + port + path + "/\n\t" +
                         "------------------------------------------------------------");
 
+    }
+
+    @RestController
+    public static class DefaultController {
+        @RequestMapping("/")
+        public String welcome() {
+            return "working in progress";
+        }
     }
 
 }
