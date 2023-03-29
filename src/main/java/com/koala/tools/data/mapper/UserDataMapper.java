81449cfb7d68fde4f1611d6dc53f9088dc79286f
KoalaBear <owen000814@outlook.com>
2023-03-29 11:00:34 +0800

    feat: update

--- src/main/java/com/koala/tools/data/mapper/UserDataMapper.java
+++ src/main/java/com/koala/tools/data/mapper/UserDataMapper.java
@@ -1,10 +1,13 @@
 package com.koala.tools.data.mapper;
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.koala.tools.data.dataModel.userData.UserDataTable;
+
 /**
  * @author koala
  * @version 1.0
  * @date 2023/3/28 21:19
  * @description
  */
-public class UserDataMapper {
+public interface UserDataMapper extends BaseMapper<UserDataTable> {
 }
