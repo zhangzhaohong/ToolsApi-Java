7bb2b2b9172f66d737f823500c2bf5e13e5187a0
KoalaBear <owen000814@outlook.com>
2023-04-23 10:46:23 +0800

    feat: update

--- src/main/java/com/koala/tools/models/douyin/v1/roomInfoData/Image.java
+++ src/main/java/com/koala/tools/models/douyin/v1/roomInfoData/Image.java
@@ -1,14 +1,12 @@
-/**
-  * Copyright 2023 json.cn 
-  */
+
 package com.koala.tools.models.douyin.v1.roomInfoData;
 import java.util.List;
 
 /**
- * Auto-generated: 2023-04-22 21:51:0
- *
- * @author json.cn (i@json.cn)
- * @website http://www.json.cn/java2pojo/
+ * @author koala
+ * @version 1.0
+ * @date 2023/4/22 21:51
+ * @description
  */
 public class Image {
 
--- src/main/java/com/koala/tools/models/douyin/v1/roomInfoData/More_panel_info.java
+++ src/main/java/com/koala/tools/models/douyin/v1/roomInfoData/More_panel_info.java
@@ -1,13 +1,11 @@
-/**
-  * Copyright 2023 json.cn 
-  */
+
 package com.koala.tools.models.douyin.v1.roomInfoData;
 
 /**
- * Auto-generated: 2023-04-22 21:51:0
- *
- * @author json.cn (i@json.cn)
- * @website http://www.json.cn/java2pojo/
+ * @author koala
+ * @version 1.0
+ * @date 2023/4/22 21:51
+ * @description
  */
 public class More_panel_info {
 
