518f42981c3684779a3565d4242f2c857bda5c1c
KoalaBear <owen000814@outlook.com>
2023-04-25 13:05:00 +0800

    feat: update

--- src/main/java/com/koala/tools/controller/DouYinVideoController.java
+++ src/main/java/com/koala/tools/controller/DouYinVideoController.java
@@ -11,12 +11,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
+import static com.koala.tools.enums.DouYinResponseEnums.UNAVAILABLE_PLAYER;
+import static com.koala.tools.enums.DouYinResponseEnums.UNSUPPORTED_OPERATION;
+import static com.koala.tools.utils.RespUtil.formatRespData;
+
 /**
  * @author koala
  * @version 1.0
  * @date 2023/4/24 11:18
  * @description
  */
+@SuppressWarnings("AlibabaUndefineMagicConstant")
 @Controller
 @RequestMapping("tools/DouYin/pro/player")
 public class DouYinVideoController {
@@ -24,13 +29,18 @@ public class DouYinVideoController {
     private static final Logger logger = LoggerFactory.getLogger(DouYinVideoController.class);
 
     @GetMapping("/video")
-    public String video(@RequestParam(value = "title", required = false, defaultValue = "VideoPlayer") String title, @RequestParam(value = "livePath", required = false) String livePath, Model model, HttpServletRequest request) {
+    public String video(@RequestParam(value = "title", required = false, defaultValue = "VideoPlayer") String title, @RequestParam(value = "livePath", required = false) String livePath, @RequestParam(value = "version", required = false, defaultValue = "2") String version, Model model, HttpServletRequest request) {
         String itemTitle = "VideoPlayer".equals(title) ? title : new String(Base64Utils.decodeFromUrlSafeString(title));
         String url = new String(Base64Utils.decodeFromUrlSafeString(livePath));
         logger.info("[videoPlayer] itemTitle: {}, inputUrl: {}, Sec-Fetch-Dest: {}", itemTitle, url, request.getHeader("Sec-Fetch-Dest"));
         model.addAttribute("title", itemTitle);
         model.addAttribute("path", url);
-        return "video/index";
+        if ("2".equals(version)) {
+            return "video/plyr/index";
+        } else if ("1".equals(version)) {
+            return "video/video.js/index";
+        }
+        return formatRespData(UNAVAILABLE_PLAYER, null);
     }
 
     @GetMapping("/live")
