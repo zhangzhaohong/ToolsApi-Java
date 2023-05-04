4d8460a1e713a4ff61c3f4e9008202995ad3c2c9
KoalaBear <owen000814@outlook.com>
2023-05-04 11:03:47 +0800

    feat: update

--- src/main/java/com/koala/tools/config/SecurityConfiguration.java
+++ src/main/java/com/koala/tools/config/SecurityConfiguration.java
@@ -2,7 +2,9 @@ package com.koala.tools.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -12,13 +14,14 @@ import org.springframework.security.web.SecurityFilterChain;
  * @description
  */
 @Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration {
 
     @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
-                .authorizeRequests().anyRequest().permitAll()
-                .and().csrf().disable().build();
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests((authorizeRequests) -> authorizeRequests.anyRequest().permitAll())
+                .csrf().disable().build();
     }
 
 }
