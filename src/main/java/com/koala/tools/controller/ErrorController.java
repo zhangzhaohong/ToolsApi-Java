aed4dbd3d9b1e8988e8c2913375c9c5241c4e8ea
KoalaBear <owen000814@outlook.com>
2023-05-17 22:46:49 +0800

    feat: update

---
+++ src/main/java/com/koala/tools/controller/ErrorController.java
@@ -0,0 +1,28 @@
+package com.koala.tools.controller;
+
+import com.koala.tools.http.annotation.HttpRequestRecorder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author koala
+ * @version 1.0
+ * @date 2023/5/17 22:30
+ * @description
+ */
+@Controller
+@RequestMapping("error")
+public class ErrorController {
+    @HttpRequestRecorder
+    @GetMapping("404")
+    public String pageNotFound() {
+        return "404/index";
+    }
+
+    @HttpRequestRecorder
+    @GetMapping("500")
+    public String internalServerError() {
+        return "500/index";
+    }
+}
