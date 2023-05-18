6d1e846a9eb3173fff2747f889aac14b0e0d43a2
KoalaBear <owen000814@outlook.com>
2023-05-18 11:43:23 +0800

    feat: update

--- src/main/java/com/koala/tools/controller/ErrorController.java
+++ src/main/java/com/koala/tools/controller/ErrorController.java
@@ -2,8 +2,10 @@ package com.koala.tools.controller;
 
 import com.koala.tools.http.annotation.HttpRequestRecorder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * @author koala
@@ -14,6 +16,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("error")
 public class ErrorController {
+
+    @HttpRequestRecorder
+    @GetMapping("403")
+    public String pageForbidden(@RequestParam(value = "notice", required = false) String notice, Model model) {
+        model.addAttribute("notice", notice);
+        return "403/index";
+    }
+
     @HttpRequestRecorder
     @GetMapping("404")
     public String pageNotFound() {
