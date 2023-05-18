44b6d921e7f30ae7ddfec118c9601f9abc3114cb
KoalaBear <owen000814@outlook.com>
2023-05-18 14:03:11 +0800

    feat: update

--- src/main/java/com/koala/tools/config/ErrorConfig.java
+++ src/main/java/com/koala/tools/config/ErrorConfig.java
@@ -23,7 +23,7 @@ public class ErrorConfig implements ErrorPageRegistrar {
           参数1 响应状态码  NOT_FOUND 404  INTERNAL_SERVER_ERROR 500
           参数2 出现响应状态码的时候的跳转路径  可以自定义跳转路径
          */
-        ErrorPage error403 = new ErrorPage(HttpStatus.FORBIDDEN, "/error/403?notice=请稍后重试");
+        ErrorPage error403 = new ErrorPage(HttpStatus.FORBIDDEN, "/error/403");
         ErrorPage error404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error/404");
         ErrorPage error500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error/500");
 
