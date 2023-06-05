51a67cf6618e64820bccbc0034cdbf74fe49fcf9
KoalaBear <owen000814@outlook.com>
2023-06-05 10:56:31 +0800

    feat: update

--- src/main/java/com/koala/tools/controller/ErrorController.java
+++ src/main/java/com/koala/tools/controller/ErrorController.java
@@ -1,9 +1,12 @@
 package com.koala.tools.controller;
 
 import com.koala.tools.http.annotation.HttpRequestRecorder;
+import org.apache.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * @author koala
@@ -17,19 +20,22 @@ public class ErrorController {
 
     @HttpRequestRecorder
     @GetMapping("403")
-    public String pageForbidden() {
+    public String pageForbidden(HttpServletResponse response) {
+        response.setStatus(HttpStatus.SC_FORBIDDEN);
         return "403/index";
     }
 
     @HttpRequestRecorder
     @GetMapping("404")
-    public String pageNotFound() {
+    public String pageNotFound(HttpServletResponse response) {
+        response.setStatus(HttpStatus.SC_NOT_FOUND);
         return "404/index";
     }
 
     @HttpRequestRecorder
     @GetMapping("500")
-    public String internalServerError() {
+    public String internalServerError(HttpServletResponse response) {
+        response.setStatus(HttpStatus.SC_INTERNAL_SERVER_ERROR);
         return "500/index";
     }
 }
