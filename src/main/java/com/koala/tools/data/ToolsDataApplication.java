2e16b1044526d86731cb962666b08507011dbb3e
KoalaBear <owen000814@outlook.com>
2023-06-13 19:54:45 +0800

    feat: 初步提交

---
+++ src/main/java/com/koala/tools/data/ToolsDataApplication.java
@@ -0,0 +1,13 @@
+package com.koala.tools.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolsDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToolsDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/koala/tools/data/ToolsDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koala.tools.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolsDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
