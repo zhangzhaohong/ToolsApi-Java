e053a4c1f434b348ab269c51a1a3c2f09c17689f
KoalaBear <owen000814@outlook.com>
2023-06-14 10:38:19 +0800

    init

---
+++ tools-base/src/test/java/com/koala/base/ToolsBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koala.base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolsBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ tools-data/src/test/java/com/koala/data/ToolsDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koala.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolsDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
