849c958eae72fb6ad52c53a545090e186cb806c6
KoalaBear <owen000814@outlook.com>
2023-06-14 18:13:02 +0800

    feat: update

---
+++ tools-factory/src/main/java/com/koala/factory/ToolsFactoryApplication.java
@@ -0,0 +1,13 @@
+package com.koala.factory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolsFactoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToolsFactoryApplication.class, args);
+    }
+
+}
---
+++ tools-factory/src/test/java/com/koala/factory/ToolsFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koala.factory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolsFactoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
