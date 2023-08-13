fbd403041fa5f35fcf0e57b836f4cc684431fc04
koala bear <owen000814@outlook.com>
2023-08-13 15:44:51 +0800

    feat: update

--- tools-factory/src/main/java/com/koala/factory/extra/netease/NeteaseCookieUtil.java
+++ tools-factory/src/main/java/com/koala/factory/extra/netease/NeteaseCookieUtil.java
@@ -99,6 +99,9 @@ public class NeteaseCookieUtil {
             if ((Double) data.get("code") == 200) {
                 List<String> cookieData = responseEntity.getHeaders().get("Set-Cookie");
                 StringBuilder cookieString = new StringBuilder();
+                if (!Objects.isNull(cookieData) && cookieData.stream().noneMatch(item -> item.startsWith("__csrf"))) {
+                    cookieString.append("__csrf=").append(PatternUtil.matchData("__csrf=(.*?);", cookieContent)).append(";");
+                }
                 if (!Objects.isNull(cookieData) && cookieData.stream().noneMatch(item -> item.startsWith("MUSIC_U"))) {
                     cookieString.append("MUSIC_U=").append(PatternUtil.matchData("MUSIC_U=(.*?);", cookieContent)).append(";");
                 }
